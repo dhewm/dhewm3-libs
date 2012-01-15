4472eb6f8e43c9d434a450bf32b20af4af829b89
dhewg <dhewg@wiibrew.org>
2012-01-15 13:23:29 +0100

    Fix compilation for MSVC
    curlbuild.h comes from the mingw build process, which includes
    inttypes.h that does not exist for MSVC.
    
    Reported by Ensiform.

--- i686-w64-mingw32/include/curl/curlbuild.h
+++ i686-w64-mingw32/include/curl/curlbuild.h
@@ -140,7 +140,7 @@
 
 /* Configure process defines this to 1 when it finds out that system  */
 /* header file inttypes.h must be included by the external interface. */
-#define CURL_PULL_INTTYPES_H 1
+//#define CURL_PULL_INTTYPES_H 1
 #ifdef CURL_PULL_INTTYPES_H
 #  include <inttypes.h>
 #endif
